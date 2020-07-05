551c4399d2812e4f81ba20ac3812b20a77e60dda
maihanh97 <58378501+maihanh97@users.noreply.github.com>
2020-07-05 18:28:17 +0700

    java

---
+++ BACKEND/src/VN/T3H/BACEND/ChuaBaiLs2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package VN.T3H.BACEND;
+
+/**
+ * @author Admin
+ *
+ */
+public class ChuaBaiLs2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
