9cf754e3e55ef3583587318ae0cf147079cbc381
maihanh97 <58378501+maihanh97@users.noreply.github.com>
2020-07-06 09:42:15 +0700

    Add files via upload

--- BACKEND/src/VN/T3H/BACEND/ChuaBaiLs2.java
+++ BACKEND/src/VN/T3H/BACEND/ChuaBaiLs2.java
@@ -1,19 +1,22 @@
-/**
- * 
- */
-package VN.T3H.BACEND;
+package VN.T3H.Session1;
 
-/**
- * @author Admin
- *
- */
+import java.util.Scanner;
+
 public class ChuaBaiLs2 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int sum =0;
+		int a=0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhập vào b = ");
+		int b = sc.nextInt();
+		while(a<b) {
+			sum += a;
+			a++;
+		}
+		System.out.println("Tổng các số nh�? hơn "+b+" = "+sum);
+		
 
 	}
 
