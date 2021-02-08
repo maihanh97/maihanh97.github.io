23ac3c7b2dff4f3f901cba3ac821dfde9cf97c72
lethuyhien <lethuyhien1410@gmail.com>
2021-02-08 17:55:06 +0700

    080221

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
 import MainScreen from './src/screens/MainScreen';
+import SlashScreen from './src/screens/SlashScreen';
 
 function App() {
-  return <MainScreen />;
+  const [time, setTime] = React.useState(false);
+  return (
+    <>
+      {setTimeout(function () {
+        setTime(true);
+      }, 3000)}
+      {time ? <MainScreen /> : <SlashScreen />}
+    </>
+  );
 }
 
 export default App;
---
+++ MyApp/src/screens/SlashScreen/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import {Text, View, StyleSheet, Image} from 'react-native';
+import {LOGO_APP} from '../../assets';
+
+const SlashScreen = () => {
+  return (
+    <View>
+      <Image source={LOGO_APP} />
+    </View>
+  );
+};
+
+export default SlashScreen;
